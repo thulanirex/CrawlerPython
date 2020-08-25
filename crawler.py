f439722787b1573729f01a9b1daf0fee3e2ddde9
thulanirex <rexroony@gmail.com>
2020-08-25 15:33:46 +0200

    Add files via upload

---
+++ crawler.py
@@ -0,0 +1,10 @@
+class DatabasePipeline(object):
+
+    def __init__(self, db, user, passwd, host):
+        self.db = db
+        self.user = user
+        self.passwd = passwd
+        self.host = host
+
+    def process_item(self, item, spider):
+        return item
